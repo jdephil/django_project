799c8b79bb777a26cb4c13bc89ed02b8d7b5227a
jdephil <jennifer.dephillips@gmail.com>
2021-03-24 14:13:57 -0400

    created static files, images and css

--- first_app/views.py
+++ first_app/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    my_dict = {'insert_me': "Hello i am from views.py"}
-    return render(request, 'index.html', context=my_dict)
+    my_dict = {'insert_me': "Hello i am from firstapp/views.py"}
+    return render(request, 'first_app/index.html', context=my_dict)
--- first_project/settings.py
+++ first_project/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(__file__)
+
 TEMPLATE_DIR = Path(BASE_DIR)/'templates'
+STATIC_DIR = Path(BASE_DIR)/'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
